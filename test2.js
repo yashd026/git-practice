86809935eb24d54e599d6be6a00679007efa7b70
vishesh-shrivastava <vishesh.shrivastava@rapidops.co>
2024-04-27 19:42:17 +0530

    test2 file changed by vishesh in vishesh branch first

--- test2.js
+++ test2.js
@@ -2,7 +2,7 @@ import React, { useCallback } from 'react'
 
 function test2() {
     const run = useCallback(() => {
-        console.log('test2');
+        console.log('test2 new');
     })
     return (
         <div>test2</div>
